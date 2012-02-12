667d94cfd3282702b86a09e9ae058c2adf496737
Luke Hudson <lukeletters@gmail.com>
2012-02-12 01:00:31 -0600

    Tidy and add features

--- strandd.pl
+++ strandd.pl
@@ -12,138 +12,130 @@ use DBI;
 
 our %relMap = (); # For randReln and --maptype
 
-sub randNum {
-	return int(rand(1024));
-}
-
-sub randHtmlText {
-	my ($fld, $len) = @_;
-	return ipsum($len, $len > 300, 1);
-}
-
-sub randText {
-	my ($fld, $len, $html) = @_;
-	return ipsum($len, $len > 300, $html);
-}
-
-sub randReln {
-	my ($fld) = @_;
-	our $mydb;
-	my $tblname = $fld;
-	$tblname =~ s/ID$//;
-	if ($relMap{$tblname}) {
-		$tblname = $relMap{$tblname};
-	}
-	eval {
-		my $img = $mydb->selectrow_hashref(qq{ SELECT ID FROM $tblname ORDER BY RAND() LIMIT 1 });
-		return $img->{ID};
-	}
-}
-
-sub randImage {
-	our $mydb;
-	my $img = $mydb->selectrow_hashref(q{ SELECT ID FROM File WHERE ClassName='Image' ORDER BY RAND() LIMIT 1 });
-	return $img->{ID};
-}
-
-sub randDatetime {
-	my $nd = int(rand(180)) - 90;
-	my $fp = (rand() < 0.5) ? 'ago' : 'future';
-	return	UnixDate(ParseDate("$nd days $fp"), '%Y-%m-%d %H:%M:%S');
-}
-sub randDate {
-	my $nd = int(rand(180)) - 90;
-	my $fp = (rand() < 0.5) ? 'ago' : 'future';
-	return	UnixDate(ParseDate("$nd days $fp"), '%Y-%m-%d');
-}
-sub randTimestamp {
-	my $nd = int(rand(180)) - 90;
-	my $fp = (rand() < 0.5) ? 'ago' : 'future';
-	return	UnixDate(ParseDate("$nd days $fp"), '%o');
-}
-
 our $opt = Getopt::Declare->new(q'[strict]
-	--class <CLASS>			Specify class to create (default Page)
-							[required]
-
-	--num <NUM>				How many objects to create.
-							[required]
+	Database connection parameters (required)
 
 	--db <DBNAME>			Specify DB name (used to find field names & types)
+							[required]
+
 	--user <DBUSER>			DB username
+							[required]
+
 	--pass <DBPASS>			DB pass
+							[required]
 
-	--columns <COLSPEC>		Add extra (text for now) columns (these are comma-separated names, with optional :Type eg  Created:Datetime, Content:Text, ...).
 
-	--skipcol <COLSPEC>		Skip these columns from the output
+	Field control options
 
-	--maptype <TMAP>		Map types for relations,  form is  FieldName:DBTable  eg  Author:Member
+	--columns <COLSPEC>		Add extra (or override) columns
+							These are comma-separated fields in the format  NAME[:TYPE:LENGTH]
+							E.G Created:Datetime,Content:HtmlText,Name:Text:30
+							Length is in chars, or is INT length otherwise (latter not impl.)
+							Column Type is one of:
+								Num - Int
+								HtmlText - Lorem ipsum with <p> tags
+								Text - Lorem ipsum with \n
+								Phrase - Words from /etc/dictionaries-common/words combined by spaces
+								Email - Words from same file, made into email
+								Reln -- Random relation ID from DB table.  See also --maptable
+								Image -- Random image from File table
+								Datetime
+								Date
+								Timestamp
+								Enum -- Random option from Enum def.  This requires the field to be defined in the table..
+
+	--exclude <COLSPEC>		Skip these columns from the output
+
+	--include <COLSPEC>		Only include these columns in the output
+
+	--maptable <TMAP>		Map relation to table. form is  FieldName:DBTable  eg  Author:Member, or Page:SiteTree
+
+
+	Miscellaneous options
 
 	--dir <DIR>				Specify out-dir
+
+	--verbose				The usual!  Output to STDERR
+
+	REQUIRED VALUES
+
+	<CLASS>					Specify class to create (default Page)
+							[required]
+
+	<NUM>					How many objects to create.
+							[required]
+
 ');
 
 croak unless $opt;
 
+# Connect to DB
 our $mydb = DBI->connect("DBI:mysql:host=localhost;database=$opt->{'--db'}",
 		$opt->{'--user'}, $opt->{'--pass'},
-		{RaiseError=>1, PrintError=>1}
+		{RaiseError=>0, PrintError=>1}
 	) or croak;
 
+our $class = $opt->{'<CLASS>'};
+our $fieldDef = $mydb->selectall_hashref(qq{ DESC $class }, 'Field');
 
-my $class = $opt->{'--class'};
-my $tbl = $mydb->selectall_hashref(qq{ DESC $class }, 'Field');
-
-my $out = {
+our $out = {
 	$class => []
 };
 
-my @keys = sort keys %$tbl;
+our $VERBOSE = $opt->{'--verbose'};
+
+=pod
+
+Sort out columns which we will output
+
+=cut
+
+my %kDefined = map { $_ => 1 } keys %$fieldDef;
+
 if ($opt->{'--columns'}) {
 	my @col = split(',', $opt->{'--columns'});
 	for my $c (@col) {
 		my ($cn, $ct, $len) = split(':', $c);
+		$fieldDef->{$cn} ||= {};
 		if ($ct) {
-			$tbl->{$cn} = { 'UserType' => $ct };
+			$fieldDef->{$cn}->{'UserType'} = $ct;
 		}
 		if ($len) {
-			$tbl->{$cn} = { 'UserLen' => $len };
+			$fieldDef->{$cn}->{'UserLen'} = $len;
 		}
-		push @keys, $cn;
+		$kDefined{$cn} = 1;
 	}
 }
+delete $kDefined{ID};
+my @keys = sort keys %kDefined;
 
-if ($opt->{'--skipcol'}) {
-	my %col = map { $_ => 1 } split(',', $opt->{'--skipcol'});
+if ($opt->{'--include'}) {
+	my %col = map { $_ => 1 } split(',', $opt->{'--include'});
+	@keys = grep { $col{$_} } @keys;
+}
+if ($opt->{'--exclude'}) {
+	my %col = map { $_ => 1 } split(',', $opt->{'--exclude'});
 	@keys = grep { !$col{$_} } @keys;
 }
 
-if ($opt->{'--maptype'}) {
-	my @col = split(',', $opt->{'--maptype'});
+if ($opt->{'--maptable'}) {
+	my @col = split(',', $opt->{'--maptable'});
 	for my $c (@col) {
 		my ($fld, $type) = split(':', $c);
 		$relMap{$fld} = $type;
 	}
 }
 
-print STDERR Dumper(\$tbl);
+print STDERR Dumper(\$fieldDef) if $VERBOSE;
+print STDERR Dumper(\@keys) if $VERBOSE;
 
-for(my $i=0; $i < $opt->{'--num'}; $i++) {
+for(my $i=0; $i < $opt->{'<NUM>'}; $i++) {
 	my $obj = {};
-FIELD: for my $f (@keys) {
-		if ($f =~ /Image.*ID$/) {
-			$obj->{$f} = randImage($f);
-			next FIELD;
-		} elsif ($f =~ /.+ID$/) {
-			$obj->{$f} = randReln($f);
-			next FIELD;
-		} elsif ($f =~ /ID$/) {
-			next FIELD;
-		}
-		my $fn = 'rand' . fieldType($tbl, $f);
-		#print $f, ' ', $fn . "\n";
-		my $val = eval { &$fn($f, fieldLen($tbl, $f)) };
-		#print " => $val\n";
-		$obj->{$f} = $val;
+FIELD: for my $field (@keys) {
+		my $func = 'rand' . fieldType($fieldDef, $field);
+		printf STDERR ("[%20s] (%s)\n", $field, $func) if $VERBOSE;
+		my $val = eval { &$func($field, fieldLen($fieldDef, $field)) };
+		$obj->{$field} = $val;
 	}
 	push @{$out->{$class}}, $obj;
 }
@@ -154,20 +146,27 @@ $mydb->disconnect();
 #print "Done.\n";
 
 sub fieldType {
-	my ($tbl, $fld) = @_;
-	if ($tbl->{$fld}) {
-		if ($tbl->{$fld}->{UserType}) {
-			return $tbl->{$fld}->{UserType};
+	my ($fieldDef, $fld) = @_;
+	if ($fieldDef->{$fld}) {
+		if ($fieldDef->{$fld}->{UserType}) {
+			return $fieldDef->{$fld}->{UserType};
 		}
 		# Spec cases.
 		for($fld) {
 			/^Content$/ && do { return 'HtmlText' };
+			/^Email$/ && do { return 'Email' };
+			/^Title$/ && do { return 'Phrase' };
+			/Name/i && do { return 'Name'; };
+			/Image.*ID$/ && do { return 'Image'; };
+			/ID$/ && do { return 'Reln'; };
 		}
-		unless ($tbl->{$fld}->{Type}) {
+
+		unless ($fieldDef->{$fld}->{Type}) {
 			return 'Text';
 		}
 
-		for($tbl->{$fld}->{Type}) {
+		for($fieldDef->{$fld}->{Type}) {
+			/^enum/ && do { return 'Enum'; };
 			/^int/ && do { return 'Num'; };
 			/^(varchar|text|mediumtext|char)/ && do { return 'Text'; };
 			/^datetime$/ && do { return 'Datetime'; };
@@ -179,19 +178,19 @@ sub fieldType {
 }
 
 sub fieldLen {
-	my ($tbl, $fld) = @_;
+	my ($fieldDef, $fld) = @_;
+	if ($fieldDef->{$fld}->{UserLen}) {
+		return $fieldDef->{$fld}->{UserLen};
+	}
 	for($fld) { # Special cases
 		/^Title$/ && do { return 30; };
 		/^MenuTitle$/ && do { return 30; };
 		/^URLSegment$/ && do { return 16; };
 	}
-	if ($tbl->{$fld}->{UserLen}) {
-		return $tbl->{$fld}->{UserLen};
-	}
-	unless($tbl->{$fld} && $tbl->{$fld}->{Type}) {
+	unless($fieldDef->{$fld} && $fieldDef->{$fld}->{Type}) {
 		return 512;
 	}
-	if ($tbl->{$fld}->{Type} =~ /^\w+\((\d+)\)/) {
+	if ($fieldDef->{$fld}->{Type} =~ /^\w+\((\d+)\)/) {
 		return $1;
 	}
 	return 1024; # rand
@@ -224,6 +223,120 @@ sub ipsum {
 	return ($html) ? '<p>' . $text . '</p>' : $text;
 }
 
+
+our @words;
+
+sub getWords {
+	our @words;
+	unless(@words) {
+		open (my $fh, '<', '/etc/dictionaries-common/words')
+			or croak $!;
+		local $/;
+		@words = split("\n", <$fh>);
+		close $fh;
+	}
+	return $words[int(rand(@words))];
+}
+
+sub randNum {
+	return int(rand(1024));
+}
+
+sub randHtmlText {
+	my ($fld, $len) = @_;
+	return ipsum($len, $len > 300, 1);
+}
+
+sub randText {
+	my ($fld, $len, $html) = @_;
+	return ipsum($len, $len > 300, $html);
+}
+
+sub randName {
+	my ($fld, $chars, $words) = @_;
+	$chars ||= 30;
+	$words ||= $chars / 5;
+	my $text = '';
+	for(my $i=0; length($text) < $chars && $i < $words; $i++) {
+		$text .= ' ' . ucfirst(getWords());
+	}
+	$text =~ s/[^\w\s]//g;
+	$text =~ s/^\s+|\s+$//;
+	return $text;
+}
+
+sub randPhrase {
+	my ($fld, $chars) = @_;
+	$chars ||= 30;
+	my $text = '';
+	for(my $i=0; length($text) < $chars; $i++) {
+		$text .= ' ' . getWords();
+	}
+	$text =~ s/^\s+|\s+$//;
+	return ucfirst $text;
+}
+
+sub randEmail {
+	my ($fld, $chars) = @_;
+	$chars ||= 30;
+	my $text = getWords();
+	$text .= '@' . getWords() . '.com';
+	$text =~ s/[^\w@.]//g;
+	$text = lc $text;
+	return $text;
+}
+
+sub randReln {
+	my ($fld) = @_;
+	our $mydb;
+	my $table = $fld;
+	$table =~ s/ID$//;
+	if ($relMap{$table}) {
+		$table = $relMap{$table};
+	}
+	eval {
+		my $tblDef = $mydb->selectall_hashref(qq{ DESC $table }, 'Field');
+		return unless $tblDef;
+		my $img = $mydb->selectrow_hashref(qq{ SELECT ID FROM $table ORDER BY RAND() LIMIT 1 });
+		return $img->{ID};
+	}
+}
+
+sub randImage {
+	our $mydb;
+	my $img = $mydb->selectrow_hashref(q{ SELECT ID FROM File WHERE ClassName='Image' ORDER BY RAND() LIMIT 1 });
+	return $img->{ID};
+}
+
+sub randDatetime {
+	my $nd = int(rand(180)) - 90;
+	my $fp = (rand() < 0.5) ? 'ago' : 'future';
+	return	UnixDate(ParseDate("$nd days $fp"), '%Y-%m-%d %H:%M:%S');
+}
+sub randDate {
+	my $nd = int(rand(180)) - 90;
+	my $fp = (rand() < 0.5) ? 'ago' : 'future';
+	return	UnixDate(ParseDate("$nd days $fp"), '%Y-%m-%d');
+}
+sub randTimestamp {
+	my $nd = int(rand(180)) - 90;
+	my $fp = (rand() < 0.5) ? 'ago' : 'future';
+	return	UnixDate(ParseDate("$nd days $fp"), '%o');
+}
+
+
+sub randEnum {
+	my ($fld) = @_;
+	our $fieldDef;
+	if ($fieldDef->{$fld}->{Type} && 
+		$fieldDef->{$fld}->{Type} =~ /^enum/) {
+
+		my $endef = $fieldDef->{$fld}->{Type};
+		$endef =~ s/^enum\(|\)$//;
+		@$endef = map { $_ =~ s/'//g; return $_; } split(/,/, $endef);
+		return $endef->[int(rand(@$endef))];
+	}
+}
 __END__
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, impedit voluptate
 deserunt blanditiis eligendi eos mollit, quis eu excepteur at, aliquam atque
